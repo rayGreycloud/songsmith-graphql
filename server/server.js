a734b4c6f583af7b2e3afd4af4e148a73587111d
rayGreycloud <rayGreycloud@gmail.com>
2017-02-21 07:25:15 -0800

    Add mLab url

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://Spock:Nimoy@ds157439.mlab.com:57439/songsmithdb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
