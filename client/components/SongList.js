07398680263fe87903b763f7167b83374caf5ad9
rayGreycloud <rayGreycloud@gmail.com>
2017-02-22 06:44:43 -0800

    Bond songs query to SongList component

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+// Help bond query to component
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
   render() {
+    console.log(this.props);
+
     return (
       <div>SongList</div>
     );
   }
 }
 
+// Define query for component
 const query = gql`
   {
     songs {
@@ -17,4 +22,6 @@ const query = gql`
   }
 `;
 
-export default SongList;
+// Bond query to component
+// Returns function thats called by SongList
+export default graphql(query)(SongList);
