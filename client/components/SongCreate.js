817355c54cc17b802ff93b2b39ab88a1dce437dc
rayGreycloud <rayGreycloud@gmail.com>
2017-02-23 07:37:34 -0800

    Add form to SongCreate

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
 class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { title: ''};
+  }
+
   render() {
     return (
       <div>
-        <h3>Create a new Song</h3>
-        <form></form>
+        <h4>Create a new Song</h4>
+        <form>
+          <label>Song Title:</label>
+          <input
+            onChange={event => this.setState({ title: event.target.value })}
+            value={this.state.title}
+          />
+        </form>
       </div>
     );
   }
