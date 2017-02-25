72c41da0ee69da58433f5ae965e48dbb508204f9
rayGreycloud <rayGreycloud@gmail.com>
2017-02-25 07:09:15 -0800

    Add data handling to input

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
 class LyricCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { content: '' };
+  }
+
   render() {
     return (
       <form>
         <label>Add New Lyric</label>
-        <input />
+        <input
+          value={this.state.content}
+          onChange={event => this.setState({ content: event.target.value })}
+        />
       </form>
     );
   }
