a82254786439fbc68c0048c78b51a118b7920b73
rayGreycloud <rayGreycloud@gmail.com>
2017-02-26 08:58:46 -0800

    Display number of likes and add styles

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -8,16 +8,19 @@ class LyricList extends Component {
   }
 
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className="collection-item">
           {content}
-          <i
-            className="material-icons md-18"
-            onClick={() => this.onLike(id)}
-          >
-            thumb_up
-          </i>
+          <div className="vote-box">
+            <i
+              className="material-icons md-18"
+              onClick={() => this.onLike(id)}
+            >
+              thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       );
     });
